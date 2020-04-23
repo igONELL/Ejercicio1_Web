bfbbf69074e781ba91b2f770996c8c650544ca54
igONELL <igorllancapan@gmail.com>
2020-04-22 20:43:02 -0400

    Ejercicio1_PWeb
    Primer ejercicio programacion web.

---
+++ webEjercicio/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet">
+    <meta name="igONELL" content="Igor Llancapan">
+
+
+    <title>CAFE</title>
+</head>
+
+<body>
+    <div class="contenedor">
+
+        <header>
+        	<img class="imagen" src="imagenes/cafe_ban.jpg">
+            <nav>
+                    <a href="http://localhost/webEjercicio/" title=>Inicio</a>
+                    <a href="http://localhost/webEjercicio/" title=>Nosotros</a>
+                    <a href="http://localhost/webEjercicio/" title=>Contacto</a>
+            </nav>
+            <h1>CAFÉ</h1>
+
+        </header>
+
+            <section>
+                <article>
+					<div class="contendorart">
+
+
+                        <h2>Articulo</h2>
+
+                        <img class="imgart" src="imagenes/cafe1.jpg">
+						<p class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum voluptatem impedit rerum, est perspiciatis architecto libero aspernatur nulla quidem. Eligendi accusamus libero ducimus necessitatibus possimus consequatur enim obcaecati, quam labore ea id. Commodi autem nobis earum molestias repellat nulla minima dolore quisquam, nostrum asperiores minus vero dolorem fugit enim consequuntur esse, quidem obcaecati tempora tempore? Tempora atque alias soluta fuga esse asperiores, dolores quidem vel architecto nihil perferendis nobis libero.</p>
+
+						<p class="tag">Tag: cafe, grano.</<p>
+						<p class="fecha">Fecha: 22-04-2020</p>
+
+                        <h2>Cafe</h2>
+
+                        <img class="imgart" src="imagenes/cafe2.jpg">
+						<p class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas obcaecati, nulla labore, voluptatum et temporibus reiciendis non sit officiis sunt consequatur similique ut in minima impedit, nihil laborum amet, totam doloremque ullam doloribus enim earum ad id dolorem. Nesciunt dolorum nobis nihil amet dolor architecto tempore incidunt rem voluptates quae, quis sapiente possimus iure illo sit repellendus quasi iste nostrum, quas earum unde et. Molestias placeat modi ut fugiat aliquid iure rerum quaerat veniam sequi veritatis. Illo quis commodi, aperiam delectus quibusdam eveniet recusandae, earum ratione eaque vero ab molestias?.</p>
+
+						<p class="tag">Tag: cafe, grano.</<p>
+						<p class="fecha">Fecha: 22-04-2020</p>
+
+
+
+
+                        <h2>Cafe</h2>
+
+                        <img class="imgart" src="imagenes/cafe3.jpg">
+                        <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure suscipit ipsum voluptatem aliquam delectus, nesciunt optio eveniet labore tempore debitis expedita et facilis esse odit impedit tenetur tempora dolorum magni earum aut omnis nostrum. Consectetur odio illo beatae facere saepe, quia earum labore nobis! Excepturi odit sequi debitis delectus quas impedit fugit doloribus neque fuga dignissimos consequuntur aperiam quia labore ad voluptate dolores aliquam unde harum, eius, commodi, ex sit, consectetur ipsum aspernatur et? Expedita pariatur laboriosam sapiente hic consequuntur?</p>
+
+						<p class="tag">Tag: cafe, grano.</<p>
+						<p class="fecha">Fecha: 22-04-2020</p>
+						</div>
+
+                </article>
+                <aside>
+                	<div class="asid">
+
+
+
+                	<h2>Asidee</h2>
+                    <p class="parrafoasid">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ab facere fugit tenetur sit. Officiis, autem recusandae dignissimos itaque illo rerum sunt eos. Id quam, ex molestias beatae. Eligendi id explicabo tempore officia aspernatur dolores assumenda animi quod vel veritatis veniam ipsum expedita accusamus amet iusto quasi, quam corrupti suscipit.</p>
+                    <p class="categorias">Categorias</p>
+
+                    <img class="imgart" src="imagenes/cafe3.jpg">
+                    <p class="parrafoasid">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    <img class="imgart" src="imagenes/cafe2.jpg">
+					</div>
+                </aside>
+            </section>
+        <footer>
+        	<div class="pie">
+
+   			<p>Desarrollado por Igor Llancapan 2020. Programación Web Santo Tomas.</p>
+			</div>
+        </footer>
+    </div>
+</body>
+
+</html>
